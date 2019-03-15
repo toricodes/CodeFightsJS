801d165a1d59bb88bd4d35c8b681f3f0755e2be8
toricodes <virodriguez.fe@gmail.com>
2019-03-15 00:42:40 -0300

    adding description of the exercise

--- Exercises/add/add.js
+++ Exercises/add/add.js
@@ -1,3 +1,7 @@
+/* Write a function that returns the sum of two numbers.
+Example:
+For param1 = 1 and param2 = 2, the output should be add(param1, param2) = 3 */
+
 const add = (param1, param2) => {
     if (typeof param1 == 'number' && typeof param2 == 'number') {
         return param1 + param2
